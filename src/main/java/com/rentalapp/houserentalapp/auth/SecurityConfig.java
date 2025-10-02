5ace6be45b0193a6f9ea7dee97eff0d030f436a8
vishalpraja777 <vishalprajapathi7@gmail.com>
2025-10-02 12:21:14 +0530

    Minor fix

--- src/main/java/com/rentalapp/houserentalapp/auth/SecurityConfig.java
+++ src/main/java/com/rentalapp/houserentalapp/auth/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         String[] unAuthenticatedEndpoints = {"/api/auth/register", "/api/auth/login", "/api/auth/**",
-                "/api/properties/get-property/*", "/api/properties/search", "/actuator/**", "/info/**"};
+                "/api/properties/get-property/*", "/api/properties/search", "/actuator/**", "/swagger-ui/**", "/info/**"};
 
         return http
                 .csrf(customizer -> customizer.disable())
